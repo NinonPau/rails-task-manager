5154dffabd52b48828fb5cf0c59145529826f355
Ninon Pau <ninon.pau@protonmail.com>
2025-08-19 17:09:10 +0200

    change the action find to show to respect Rails norms

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: "new"
   post "tasks", to: "tasks#create"
-  get "tasks/:id", to: "tasks#find", as: "task"
+  get "tasks/:id", to: "tasks#show", as: "task"
   get "tasks/:id/edit", to: "tasks#edit", as: "edit"
   patch "tasks/:id", to: "tasks#update"
 end
